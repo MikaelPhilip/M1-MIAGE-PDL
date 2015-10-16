8d4d3f2c24c739d3a9357daf7dcf02738ef944a9
12000209 <12000209@e010m02.istic.univ-rennes1.fr>
2015-10-16 10:00:32 +0200

    Ajout d'une librairie pour les graphiques et premier teste d'affichage

--- WebSite/Traitement/Traitement.js
+++ WebSite/Traitement/Traitement.js
@@ -1,1 +1,32 @@
-<!--futur méthode chargement de json et de la structure de base du site-->
+//Création d'une variable qui représente notre graphque
+function generate() {
+	//On indique ici la zone ou on fera apparaitre le graphique (id du div)
+    $('#graphique').highcharts({
+		//Type
+        chart: {
+            type: 'bar' 
+        },
+		//Titre
+        title: {
+            text: 'Consommation de fruit'
+        },
+		//Axe x
+        xAxis: {
+            categories: ['Pommes', 'Bananes', 'Oranges']
+        },
+		//Axe y
+        yAxis: {
+            title: {
+                text: 'Nombre de fruits mangés'
+            }
+        },
+        //Data
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+}
