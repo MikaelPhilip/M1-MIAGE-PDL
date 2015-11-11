3d6d3921623b667100eb2d64bd0652e756495f28
MikaelPhilip <philip.mikael@gmail.com>
2015-11-11 16:07:20 +0100

    Modification de l'exemple MyPCMPrinter.java pour créer un json.Utilisation de la librairie gson pour convertir les hashmap en JSON valide (reste un élement à convertir)

--- src/testLibrairies/MyPCMPrinter.java
+++ src/testLibrairies/MyPCMPrinter.java
@@ -3,6 +3,7 @@ package testLibrairies;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -11,19 +12,22 @@ import org.opencompare.api.java.impl.ValueImpl;
 import org.opencompare.api.java.util.PCMVisitor;
 import org.opencompare.api.java.value.*;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * Created by gbecan on 02/02/15.
  */
-public class MyPCMPrinter implements PCMVisitor {
-
-    private boolean isBooleanCell;
-    JSONArray ja = new JSONArray();
-    JSONObject obj = new JSONObject();
-
+public class MyPCMPrinter implements PCMVisitor{
+	private boolean isBooleanCell;
+    //JSONObject obj = new JSONObject();
     List l= new ArrayList();
-	HashMap<String, String> map1 = new  HashMap<String,String>();
-
-
+    Map<String, String> map = new HashMap<String,String>();
+	Map<String, String> map1 = new HashMap<String,String>();
+	public Object propone;
+	//Va servir pour convertir les hashmap (données et filtres) en json
+	//We have stack overflow because of recursivity: we must add exclusion strategie for avoid this
+	Gson gson = new GsonBuilder().create();
 
     /**
      * Print some information contained in a PCM
@@ -32,27 +36,21 @@ public class MyPCMPrinter implements PCMVisitor {
     public void print(PCM pcm) {
         boolean filters=true;
         String str_type_filter;
-        String [] str;
-        
+        String [] str;  
         
         // We start by listing the names of the products
         System.out.println("--- Products ---");
-    	HashMap<String, List> map = new  HashMap<String,List>();
-    	HashMap<String, String> filter = new  HashMap<String,String>();
-
-
         for (Product product : pcm.getProducts()) {
-           System.out.println(product.getName());
-      //  System.out.println(product.getCells());
+           // System.out.println(product.getName());
+           // System.out.println(product.getCells());
            
-           map.put("\n"+product.getName(),  product.getCells());
-      
+           
+           map.put(product.getName(), product.getCells().toString());
 			// obj.put(product.getName(),map);
             
            if(filters){
             for (Cell cell : product.getCells()) {
             
-            	
             	// l.add(cell.getFeature().getName());
             	// l.add(cell.getInterpretation()+"");
             	str_type_filter= cell.getInterpretation()+"";
@@ -64,8 +62,7 @@ public class MyPCMPrinter implements PCMVisitor {
             		str_type_filter="StringValue";
             		
             	}
-            			
-            	
+           		      	
             	 map1.put(cell.getFeature().getName(),str_type_filter);
             	 filters=false;
             }
@@ -73,15 +70,20 @@ public class MyPCMPrinter implements PCMVisitor {
             
         }
         l.add(map1);
-        map.put("filters",  l);
-
-      
+        
+        //map.put("filters",  l);
+        //On converti en format json les collections
+        String donneeJson= gson.toJson(map); 
+        String filtreJson= gson.toJson(map1); 
+        //On rajoue les string en format json
+        //obj.put("products",map);
+        //obj.put("filters", map1);
+        //Lecture json
+        //TODO: à l'implémentation au lieu d'afficher enregistrer tout cela dans un fichier .json
         System.out.println("---JSON----");
-        System.out.println(map);
-        System.out.println(map1);
-        System.out.println("---FIN JSON----");
-    //    System.out.println(obj);
-
+        System.out.println(donneeJson);
+        System.out.println(filtreJson);
+        System.out.println("---End JSON----");
         // Then, we use a visitor to print the content of the cells that represent a boolean value
         System.out.println("--- Boolean values ---");
         pcm.accept(this);
