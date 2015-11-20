0e01b8e3b3b936e12f008d929e99226910a35957
MikaelPhilip <philip.mikael@gmail.com>
2015-11-20 12:01:08 +0100

    Site:Debut methode affichage point et création dimensions Site:Correction d'un bug pour le filtre

--- WebSite/Traitement/Traitement.js
+++ WebSite/Traitement/Traitement.js
@@ -12,7 +12,6 @@ function traitement(){
 		$.ajaxSetup({ cache: false });
 	});
 	LoadJson();
-	browseJson(event);
 }
 
 //Function for load one of generates Json 
@@ -41,6 +40,7 @@ function LoadJson(){
 		json = file;
 		GenerateFilter(json);
 		Generate(json);	
+		//browseJson(json);
 	})
 	.fail(function(jqXHR, status, error){
        alert("Erreur lors du chargement: Veuillez utiliser un fichier json valide.Message: "+ error);
@@ -49,12 +49,7 @@ function LoadJson(){
 }
 //Fonction generate chart (dimension)
 function Generate(json){
-	//TODO:Création
-	//TODO:Lecture de l'objet dimension pour les axes x et y et taille/couleur
-	//TODO:Placement
-	//TODO:Appelle pour données
-	//TODO:Generation
-	// register our custom symbols to nvd3
+	/*// register our custom symbols to nvd3
 	// make sure your path is valid given any size because size scales if the chart scales.
 	nv.utils.symbolMap.set('thin-x', function(size) {
 	size = Math.sqrt(size);
@@ -62,7 +57,7 @@ function Generate(json){
 	'l' + size + ',' + size +
 	'm0,' + -(size) +
 	'l' + (-size) + ',' + size;
-	});
+	});*/
 	
 	// create the chart
 	var chart; //Var Chart
@@ -81,11 +76,11 @@ function Generate(json){
 		chart.xAxis.tickFormat(d3.format('.02f'));
 		chart.yAxis.tickFormat(d3.format('.02f'));
 		
-		//Rattachement à une balise html
+		//add chart in html
 		d3.select('#graph svg')
-		//Appel de la méthode de generation de données
-		.datum(LoadData(4,40)) //Méthode temporaire
-		//Generation du graph
+		//add data, product on chart
+		.datum(LoadData()) //Méthode temporaire
+		//chart generation
 		.call(chart);
 		nv.utils.windowResize(chart.update);
 		chart.dispatch.on('stateChange', function(e) { ('New State:', JSON.stringify(e)); });
@@ -94,15 +89,112 @@ function Generate(json){
 }				
 
 //Fonction de genration des données (ici un exemple qui genere aléatoirement
-function LoadData(groups, points) { //# groups,# points per group
-	// smiley and thin-x are our custom symbols!
-	var data = [],
+function LoadData() { //# groups,# points per group
+	// recupérerer les dimensions
+	//1ere et 2nd dimension et 3eme  et 4 éme dimension on sauvegarde le nom (servira pour récuperer valeur x et y,pour detemriner la taille, et les groupes de couleurs)
+	//si pas de 3 éme ou 4 éme on a y= undefined
+	
+	// créer les groupe (couleurs) pour la 4éme dimensions
+	//4éme dimension : determiner min et max: faire 3 valeur (<1/4 max ,<1/2 max,<3/4 max)
+	//si pas de 4 éme dimension on fait qu'un goupe
+	//si 4éme dimension:On crée 4 groupe pour la 4 éme dimensions
+	
+	//TODO: Pour chaque produit: 
+	//si on a plusieurs groupe : on récupére la valeur de la 4 éme dimension on la conpore pour saovir dans quel groupe de couleur mettre le point
+	//sinon on le rajoute dans le seul groupe
+	//aprés on indique les coordonnées x et y suivant les valeurs de la 1er et 2 nd dimension
+	//On determine la taille suivant la vlauer de la 3éme dimensin
+	//array for our dot on chart 
+	var data=[];
+	//params reprents our dimension
+	var dimX; //first dimension
+	var dimY; //second dimension
+	var dimSize; //third dimension
+	var dimColor; //fourth dimension
+	
+	var dimColorLow; //max value for first color
+	var dimColorMed; //max value for second color
+	var dimColorHigh; //max value for third color
+	
+	/*Get names of dimensions*/
+	//Get our dimension (name= name of feature, value= dimension number)
+	$.each(json["DIMENSIONS"], function(name, value) {
+		if(value==1){
+			dimX=name;
+		}else if(value==2){
+			dimY=name;
+		}else if(value==3){
+			dimSize=name;
+		}else if(value==4){
+			dimColor=name;
+		}
+	});
+	if ((typeof dimX === 'undefined')||(typeof dimY === 'undefined')){
+		alert("Erreur le json ne défini pas deux premieres dimensions");
+	}	
+	/*Create group of color for fourth dimension*/
+	//If 4th dimension exist
+	if (typeof dimColor !== 'undefined'){
+		//Define groups (for fourth dimension (color)): each groupe has got one color
+		//Determine max for dimColor feature
+		var max;
+		//for each product we get his list of features
+		$.each(json, function(i, product) {
+			if (i != "FILTERS" && i !="DIMENSIONS"){
+				var value = parseFloat(product[dimColor],10);
+				//we search value for feature == name (feature for filter)
+				//check if it's first time we search max value
+				if(typeof max === 'undefined'){
+					max=value;
+				}
+				//check if his value > max
+				if(value > max){
+					max=value;
+				}
+			}
+		});
+		//Determine limit for each group/color
+		dimColorLow= max/4;
+		dimColorMed= max/2;
+		dimColorHigh= max*3/4;
+		//Create four group in data
+		for (i = 0; i < 4; i++) {
+			data.push({
+				key: 'Group ' + i,
+				values: []
+			});
+		}
+	}else{
+	//No 4th dimension: just one color,one group
+	data.push({
+		key: 'Group1',
+			values: []
+		});
+	}
+	
+	/*Add to data each product*/
+	$.each(json, function(name, product) {
+		if (name != "FILTERS" && name !="DIMENSIONS"){
+			//If dimension 4 exist compare value to know the group where the dot will add
+			/*data[i].values.push({
+			x: random(),
+			y: random(),
+			size: Math.round(Math.random() * 100) / 100,
+			shape: shapes[j % shapes.length]
+			});*/
+		}
+	}
+	return data;
+	
+	
+	//smiley and thin-x are our custom symbols!
+	/*var data = [],
 	shapes = ['thin-x', 'circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
 	random = d3.random.normal();
 	for (i = 0; i < groups; i++) {
 		data.push({
-		key: 'Group ' + i,
-		values: []
+			key: 'Group ' + i,
+			values: []
 		});
 		for (j = 0; j < points; j++) {
 			data[i].values.push({
@@ -113,39 +205,21 @@ function LoadData(groups, points) { //# groups,# points per group
 			});
 		}
 	}
-	return data;
-	
-	/*TODO: Sanaa : Mettre en commentaire la generation automatique et commencer a traiter le json.
-	C'est à dire: paarcourir tout les objets de JSON (ne rien faire si l'objet est le filtre): et récuperer pour chaque objet le couple caractéristique/donnée et juste l'afficher (console.log)*/
+	return data;*/
 }
 
-function browseJson(event){
-	//create a reader
-	var reader = new FileReader();
-    reader.onload = onReaderLoad; //Call an internal function when reading text
-    reader.readAsText(event.target.files[0]);
-	 
-	//internal fonction for create an json obj with file and launch treatment if it's ok
-	function onReaderLoad(event){
-		try{
-			json = JSON.parse(event.target.result);
-			console.log(json);
-			// parcourir le premier tableau Json pour recuperer les cle et les objets B associe
-			for(var value in json){
-			//parcourir l objet B pour recupere les cle et les valeur qui sont dans B
-			    json2  = json[value];
-			    // Pour chaque gRand objet on affiche les petits objets cle/valeur
-			    
-			 	for(var key in json2){
-			 	 if(value != "FILTERS"){
-            		console.log(value + " "+key +" -> "+ json2[key]);
-            	  }
-     		 	}
-     		 }
-		}catch(e)
-		{
-			alert("Erreur lors du chargement: Veuillez utiliser un fichier json valide");
-		}	
+function browseJson(){
+	console.log(json);
+	// parcourir le premier tableau Json pour recuperer les cle et les objets B associe
+	for(var value in json){
+	//parcourir l objet B pour recupere les cle et les valeur qui sont dans B
+	json2  = json[value];
+	// Pour chaque gRand objet on affiche les petits objets cle/valeur	    
+	for(var key in json2){
+		if(value != "FILTERS"){
+            console.log(value + " "+key +" -> "+ json2[key]);
+        }
+    }
     }
 }
 
@@ -171,41 +245,13 @@ function GenerateFilter(json){
              +"<div class='checkbox checkbox-primary'>"
              +"<label><input type='checkbox' name=chbx"+name+" value='chbx"+name+"'>"+name+"</label>"
              +"</div>";
-		}else if (type=="IntegerValue"){
+		}else if (type=="IntegerValue" || type=="RealValue"){
 			//Search the max and min value for this filter
 			var min;
 			var max;
 			//for each product we get his list of features
 			$.each(json, function(i, product) {
-				if (i != "FILTERS" && i !="DIMENSION"){
-					var value = parseInt(product[name],10);
-					//we search value for feature == name (feature for filter)
-					//check if it's first time we search min value
-					if(typeof min === 'undefined'){
-						min=value;
-					}
-					//check if it's first time we search max value
-					if(typeof max === 'undefined'){
-						max=value;
-					}
-					//check if his value < min
-					if(value < min){
-						min=value;
-					}
-					//check if his value > max
-					if(value > max){
-						max=value;
-					}
-				}
-			});
-			contentsNumber+="<p>"+name+".Type: "+type+",min: "+min+",max: "+max+"</p>";
-		}else if (type=="RealValue"){
-			//Search the max and min value for this filter
-			var min;
-			var max;
-			//for each product we get his list of features
-			$.each(json, function(i, product) {
-				if (i != "FILTERS" && i !="DIMENSION"){
+				if (i != "FILTERS" && i !="DIMENSIONS"){
 					var value = parseFloat(product[name],10);
 					//we search value for feature == name (feature for filter)
 					//check if it's first time we search min value
@@ -236,7 +282,7 @@ function GenerateFilter(json){
 			//List of value
 			var ArrValue = [];
 			$.each(json, function(i, product) {
-				if (i != "FILTERS" && i !="DIMENSION"){			
+				if (i != "FILTERS" && i !="DIMENSIONS"){			
 					ArrValue.push(product[name]);		
 				}
 			});
