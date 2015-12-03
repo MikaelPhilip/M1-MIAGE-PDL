b0fed53616577d47f525222193dc63bb42d82d05
MikaelPhilip <philip.mikael@gmail.com>
2015-12-03 15:27:55 +0100

    DOSSIER: Completer les examples

--- src/main/execution/Main.java
+++ src/main/execution/Main.java
@@ -10,9 +10,10 @@ import main.traitement_Impl.Traitement;
  */
 public class Main {
 	
-	public static String PCMPATH="pcms/data.pcm";
-	public static String PARAMPATH="./testParameters/JSONParametres.json";
-	public static String JSONPATH="./json/generation.json";
+	//Global Paths parameters
+	public static String PCMPATH="pcms/data.pcm"; //where is the pcm to load
+	public static String PARAMPATH="./testParameters/JSONParametres.json"; //where is the json parameters 
+	public static String JSONPATH="./json/generation.json"; //where generate json will created
 	
 	public static void main(String[] args) throws IOException{	
 		//Create traitement
