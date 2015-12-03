4b1e23197257f9c1b134826d371bdeeb43aebcc8
Seifeddine <jlassi.seif@yahoo>
2015-12-03 12:06:20 +0100

    Ajout readme

--- README.md
+++ README.md
@@ -1,26 +1,70 @@
 # OpenCompare - Procuct Chart
 PDL Project - M1 Miage
 
+OpenCompare.org aims to help a community of users to import,edit, view, and use comparison matrices,typically for selecting
+products in a given field.
 
 ##Objectives
 
+This Software Development project ( PDL) has for objectives:
 
+- To understand the existing project (requirements, specificity of the technologies and the field of application, the architecture,...).
+- To write a specifications.
+- To contribute very concretely to the project both at the code level(case of tests, additions of features, refactoring..)of the documentation, or the implementation of the continuous integration.
+- To validate in a continue way the requirements and the implementation with the customer.
+- To Generate a graph and a set of filter from a table of matrices.
+- To Visualize the graph on a web page.
+- To Allow the user to interact with the graph with filters and the possibility To add dimensions.
 
 
 ##Results
 
+/*The purpose of this project is to generate from a matrix of comparison, an interactive called display(visualization) " product chart ".*/
 
+The Open project and Compare "charts product" is intended to generate, from a Comparison Matrix, an interactive display(visualization) called "product chart". That is Conceive a module which will allow to transform a table of matrix into an interactive graph.
+The Treatment will be done by reusing Java API for handling matrices.Un "product chart" consists of:
+- An placement of products on an axis in 2 dimensions with abscissas X and orderly Y. X and Y corresponding in characteristics of products (price, size, weight, performance, security,...);
+- A panel of configuration / filtering which dynamically is able to update the placement of products (and to eliminate the products which do not correspond any more to the wishes of the user).
 
 
 ##License
 
 
 
+
 ##Development tools
+
+languages
+
+- JAVA for treatment.
+- HTML / JS / CSS for display.
+
+
+library
+
+JAVA
+
+ Open Compare API: library of open pcm compared to manipulate files.
+- JUNIT 4: Unit Testing Framework for the Java language.
+- Log4js: To save the results in a journal.
+- JSON: To manipulate JSON files.
+- ANGULAR JS: open-source JavaScript Framework, which modifies and creates a page html dynamically monitors and detects changes on a JavaScript object.
+- JSON (Javascript Object Notation) : is a lightweight data-interchange format (syntax for storing and exchanging data)
+- Bootstrap: collection of tools useful for the design of sites and Web applications.
+- D3:library for displaying a graph.
+
+
+Choice of tools
+
+- For the part conception of the project,  we will use UML as language ofModelling and PowerAMC as software to generate the models and Diagrams.
+ -For the part JAVA, we use IDE Eclipse  and Maven2 to the structure project.
+- For the Web Part will be tested locally with UwAmp (a Wamp package type,combining several technologies that simplifies the creation of a Web serverWindows).
 
 
 
 ##Project Architecture 
+
+
 
 
 
--- src/test/junit/TestJava.java
+++ src/test/junit/TestJava.java
@@ -3,7 +3,6 @@ package test.junit;
 import java.io.IOException;
 import java.util.Iterator;
 
-import main.execution.Main;
 import main.generationJSON_impl.Generation;
 import main.traitement_Impl.Traitement;
 
@@ -14,115 +13,101 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Test class for traitement.java
-**/
 public class TestJava {
 
 	public Traitement traitement;
 	public Generation generation;
+	public String path;
 	public JSONObject _jsonGenere ;
-	
+	public JSONObject _json2 ;
 	/**
 	 * @throws java.lang.Exception
-	 * Initialisation
-	 **/
+	 * Méthode d'initialisation
+	 */
 	@Before
 	public void setUp() throws Exception {
 		//Créer un objet traitement
 		traitement = new Traitement();
-		generation = new Generation(traitement);
+		generation = new Generation();
+		path = generation.get_filepath();
+		//_jsonGenere = generation.get_json();
+		
 	}
 	
 	/**
-	 * Method load pcm ok
+	 * Méthode de chargement pcm : cas ok
 	 * @throws IOException 
 	 */
 	@Test
 	public void testChargementPcm1() throws IOException{
-		traitement.pcmLoad(Main.PCMPATH);
+		traitement.pcmLoad("pcms/data.pcm");
 		assertNotNull(traitement.getPcm());
 	}
 	
+	
+	
+	// test ok sur json genere
+	@Test
+	public void testCompareJsonPcm1() throws IOException{
+		traitement.pcmLoad("pcms/data.pcm");
+		assertNotNull(traitement.getPcm());
+		generation.generateJSON(traitement.getPcm());
+		_jsonGenere=generation.get_json();
+		assertTrue("Remove the first Product" , generation.verifJSONgenere(_jsonGenere,traitement.getPcm()));
+	}
+	//test sur product not ok
+	@Test
+	public void testCompareJsonPcm2() throws IOException{
+		traitement.pcmLoad("pcms/data.pcm");
+		assertNotNull(traitement.getPcm());
+		generation.generateJSON(traitement.getPcm());
+		_jsonGenere=generation.get_json();
+		Iterator<String> it =  _jsonGenere.keys();
+		_jsonGenere.remove(it.next());
+		assertFalse("Remove the first Product" , generation.verifJSONgenere(_jsonGenere,traitement.getPcm()));
+		
+	}
+	// test sur les feature not ok ;
+	@Test
+	public void testCompareJsonPcm3() throws IOException{
+		traitement.pcmLoad("pcms/data.pcm");
+		assertNotNull(traitement.getPcm());
+		_jsonGenere=generation.get_json();
+		Iterator<String> it =  _jsonGenere.keys();
+		
+		JSONObject _json22 =_jsonGenere.getJSONObject(it.next());
+		Iterator<String> it1 =  _json22.keys();
+		String s=it1.next();
+		_json2.put(s, "toto");
+		//System.out.println(traitement.getPcm());
+		assertFalse("Remove the first Product" , generation.verifJSONgenere(_json2,traitement.getPcm()));
+	}
+	
+	// test sur les cell not ok ;
+	@Test
+	public void testCompareJsonPcm4() throws IOException{
+		traitement.pcmLoad("pcms/data.pcm");
+		assertNotNull(traitement.getPcm());
+		//System.out.println(traitement.getPcm().getName());
+//		assertFalse("test sur les cell not ok  ",generation.verifJSONgenere("./testJSON/testCompareJsonPcm3.json",traitement.getPcm(),null));
+	}
+	
+	// cas json genere ok
+	@Test
+	public void testCompareJsonPcm5() throws IOException{
+		traitement.pcmLoad("pcms/data.pcm");
+		assertNotNull(traitement.getPcm());
+		//System.out.println(generation.get_filepath());
+//		assertTrue("test sur les cell not ok  ",generation.verifJSONgenere(null,traitement.getPcm(),generation.get_json()));
+	}
+	
 	/**
-	 *  Method load pcm error: file don't exist
+	 * Méthode de chargement pcm : cas fichier inexistant
 	 * @throws IOException 
-	**/
+	 */
 	@Test (expected=IOException.class)
 	public void testChargementPcm2() throws IOException{
 		traitement.pcmLoad("pcms/fail.pcm");
-	}
-	
-	/**
-	 * Method check json error: product missing
-	**/
-	@Test
-	public void testCompareJsonPcm1() throws IOException{
-		//load pcm
-		traitement.pcmLoad(Main.PCMPATH);
-		assertNotNull(traitement.getPcm());
-		//generate json
-		generation.generateJSON(traitement.getPcm());
-		_jsonGenere=generation.get_json();
-		//we delete first product
-		Iterator<String> it =  _jsonGenere.keys();
-		_jsonGenere.remove(it.next());
-		//VerifJSONgenere must send false
-		assertFalse("Remove the first Product" , generation.verifJSONgenere(_jsonGenere,traitement.getPcm()));
-	}
-	
-	
-	/**
-	 * Method check json error: feature for product missing
-	**/
-	@Test
-	public void testCompareJsonPcm2() throws IOException{
-		traitement.pcmLoad(Main.PCMPATH);
-		assertNotNull(traitement.getPcm());
-		//generate json
-		generation.generateJSON(traitement.getPcm());
-		_jsonGenere=generation.get_json();
-		//we delete one feature of first product
-		Iterator<String> it =  _jsonGenere.keys();
-		JSONObject product=_jsonGenere.getJSONObject(it.next()); //get first product
-		Iterator<String> it2 =  product.keys(); //iterator on features
-		product.remove(it2.next());  //remove first feature of first product
-		//VerifJSONgenere must send false
-		assertFalse("Remove the first feature Product" , generation.verifJSONgenere(_jsonGenere,traitement.getPcm()));
-	}
-	
-	/**
-	 *	Method check json error: bad value feature for product missing
-	 * @throws IOException
-	 */
-	@Test
-	public void testCompareJsonPcm3() throws IOException{
-		traitement.pcmLoad(Main.PCMPATH);
-		assertNotNull(traitement.getPcm());
-		//generate json
-		generation.generateJSON(traitement.getPcm());
-		_jsonGenere=generation.get_json();
-		//we change value one feature of first product
-		Iterator<String> it =  _jsonGenere.keys();
-		JSONObject product=_jsonGenere.getJSONObject(it.next()); //get first product
-		Iterator<String> it2 =  product.keys(); //iterator on features
-		product.put(it2.next(),"toto"); //change value for first feature
-		assertFalse("Bad value for first feature" , generation.verifJSONgenere(_jsonGenere,traitement.getPcm()));
-	}
-	
-	/**
-	 * Methode check json ok
-	 * @throws IOException
-	 */
-	@Test
-	public void testCompareJsonPcm4() throws IOException{
-		traitement.pcmLoad(Main.PCMPATH);
-		assertNotNull(traitement.getPcm());
-		//generate json
-		generation.generateJSON(traitement.getPcm());
-		_jsonGenere=generation.get_json();
-		//no modification must be ok
-		assertTrue("Check Json ok ", generation.verifJSONgenere(_jsonGenere,traitement.getPcm()));
 	}
 	
 	/**
